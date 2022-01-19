18f7c44d259a402ebf6b55f58e005ffd79fc7721
AJIL K <86887880+ajil2545@users.noreply.github.com>
2022-01-19 15:55:27 +0530

    commit

--- cricketPlayers.php
+++ cricketPlayers.php
@@ -5,10 +5,10 @@
     <body>
         <?php
             $players=array("Sachin Tendulkar","M.S.Dhoni","Virat Kohli","Rohit Sharma","Yuvraj Singh","Hardik Pandya","Jasprit Bumrah","Ravindra Jadeja","Shikhar Dhawan","Rishabh Pant","Kapil Dev");
-            echo"<table align='center' style='border:1px solid black;' cellspacing='12'><tr><th>No.</th><th>Name</th></tr>";
+            echo"<table align='center' style='border:1px solid black;' cellspacing='12' width='50%'><tr><th>No.</th><th>Name</th></tr>";
             foreach($players as $key=>$value)
             {
-                echo"<tr><td>". $key+1 ."</td><td>$value</td></tr>";
+                echo"<tr><th>". $key+1 ."</th><th>$value</th></tr>";
             }
             echo"</table>";
         ?>
